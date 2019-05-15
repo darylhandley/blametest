de1291ecb91a50a0fd4d77f336554ade439b8eee
dhandley <dhandley@sonatype.com>
2019-05-14 23:24:00 -0400

    DO NOT COMMIT

--- src/main/java/com/sonatype/blametest/Constants.java
+++ src/main/java/com/sonatype/blametest/Constants.java
@@ -2,5 +2,5 @@ package com.sonatype.blametest;
 
 public class Constants {
 
-  public static String AUTH_TOKEN = "fd77d91dd3247171af51a7b36a1f441a8b0f7d80";
+  public static String AUTH_TOKEN = Config.getGithubApiKey();
 }
